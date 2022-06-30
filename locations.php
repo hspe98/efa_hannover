50bb68f11eecdda213182f0100bd76a2bfa2287a
HSpe98 <hauke.spekker@gmx.de>
2022-06-30 07:17:56 +0200

    Add files via upload
    First files (/locations, /stops/:id/departures, /stops/nearby, /stops/:id) jet without comments in code

---
+++ locations.php
@@ -0,0 +1,57 @@
+<?php
+
+include 'functions.php';
+
+if (isset($_GET['help'])) {
+    header("Content-Type: text/html");
+    echo "
+    <h1>".$_SERVER['SCRIPT_NAME']."</h1>
+    <pre>
+    q           required            Search term
+    poi         True		Get pois?
+    stops       True		Get stops?
+    addresses   True		Get addresses?
+    results     10			Number of results
+    language    en			language
+    pretty      False		Pretty-print JSON?
+    </pre>";
+} else {
+    header("Content-Type: application/json");
+    //locations
+    if (isset($_GET['q'])) {
+        $pQ = $_GET['q'];
+    } else {
+        exit("Error! Set q or set help for help");
+    }
+    if (isset($_GET['poi'])) {
+        $gPOI = filter_var($_GET['poi'], FILTER_VALIDATE_BOOLEAN);
+    } else {
+        $gPOI = True;
+    }
+    if (isset($_GET['stops'])) {
+        $gStops = filter_var($_GET['stops'], FILTER_VALIDATE_BOOLEAN);
+    } else {
+        $gStops = True;
+    }
+    if (isset($_GET['addresses'])) {
+        $gStreets = filter_var($_GET['addresses'], FILTER_VALIDATE_BOOLEAN);
+    } else {
+        $gStreets = True;
+    }
+    if (isset($_GET['results'])) {
+        $paramResults = $_GET['results'];
+    } else {
+        $paramResults = 10;
+    }
+    if (isset($_GET['language'])) {
+        $paramLang = $_GET['language'];
+    } else {
+        $paramLang = "en";
+    }
+    if (isset($_GET['pretty'])) {
+        $pPretty = $_GET['pretty'];
+    } else {
+        $pPretty = False;
+    }
+    echo utf8_decode(getLocation($pQ, $numResults=$paramResults, $language=$paramLang, $getPOI=$gPOI, $getStreets = $gStreets, $getStops = $gStops, $pretty=$pPretty));
+}
---
+++ stops_departures.php
@@ -0,0 +1,82 @@
+<?php
+include 'functions.php';
+
+header("Content-Type: application/json; charset=utf-8");
+
+//25003001 Uhlhornstraße
+if (isset($_GET['id'])) {
+    $pId = $_GET['id'];
+} else {
+    exit("Error! Set id or set help for help");
+}
+if (isset($_GET['when'])) {
+    $pWhen = $_GET['when'];
+} else {
+    $pWhen = "now";
+}
+if (isset($_GET['results'])) {
+    $pResults = $_GET['results'];
+} else {
+    $pResults = 10;
+}
+
+if (isset($_GET['suburban'])) {
+    $pSuburban = filter_var($_GET['suburban'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pSuburban = True;
+}
+if (isset($_GET['subway'])) {
+    $pSubway = filter_var($_GET['subway'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pSubway = True;
+}
+if (isset($_GET['tram'])) {
+    $pTram = filter_var($_GET['tram'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pTram = True;
+}
+if (isset($_GET['bus'])) {
+    $pBus = filter_var($_GET['bus'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pBus = True;
+}
+if (isset($_GET['ferry'])) {
+    $pFerry = filter_var($_GET['ferry'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pFerry = True;
+}
+if (isset($_GET['express'])) {
+    $pExpress = filter_var($_GET['express'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pExpress = True;
+}
+if (isset($_GET['regional'])) {
+    $pRegional = filter_var($_GET['regional'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pRegional = True;
+}
+if (isset($_GET['remarks'])) {
+    $pRemarks = filter_var($_GET['remarks'], FILTER_VALIDATE_BOOLEAN);
+} else {
+    $pRemarks = True;
+}
+
+if (isset($_GET['duration'])) {
+    $pDuration = $_GET['duration'];
+} else {
+    $pDuration = 99999999;
+}
+if (isset($_GET['direction'])) {
+    $pDirection = $_GET['direction'];
+} else {
+    $pDirection = "";
+}
+
+if (isset($_GET['pretty'])) {
+    $pPretty = $_GET['pretty'];
+} else {
+    $pPretty = False;
+}
+
+echo getStopsDeparturesById($argId=$pId, $argWhen = $pWhen, $argResults = $pResults, $argRemarks=$pRemarks, $argDirection=$pDirection, $argDuration=$pDuration, $argLinesOfStops=True, $argSuburban = $pSuburban, $argSubway = $pSubway, $argTram = $pTram, $argBus = $pBus, $argFerry = $pFerry, $argExpress = $pExpress, $argRegional = $pRegional, $argPretty=$pPretty);
+
