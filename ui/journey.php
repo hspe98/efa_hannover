6f04898f48e076eaa9ee04822da34b4191b73ad1
hauke@spekker.info <HSpe>
2022-07-25 01:03:12 +0200

    test

--- ui/journey.php
+++ ui/journey.php
@@ -451,7 +451,8 @@ Error! Set origin (from or from.latitude & from.longitude) or set help for help<
     }
     $data = getJourney($argOrigin = $pOrigin, $argDestination = $pDestination, $argCalcNumberOfTrips = $pCalcNumberOfTrips, $argRemarks = $pRemarks, $argWhen = $pWhen, $argDepOrArrTime = $pDepOrArrTime, $argMaxTransfers = $pMaxTransfers, $argWalkingSpeed = $pWalkingSpeed, $argSuburban = $pSuburban, $argSubway = $pSubway, $argTram = $pTram, $argBus = $pBus, $argFerry = $pFerry, $argExpress = $pExpress, $argRegional = $pRegional, $argLanguage = $pLanguage, $argPretty = $pPretty);
     
-    //$loc = json_decode(utf8_encode($data), 1, JSON_UNESCAPED_UNICODE);
+    $loc = json_decode(utf8_encode($data), 1, JSON_UNESCAPED_UNICODE);
+    print_r($loc);
     $already_got_from = '<h4>' . STR_SEARCH_1_1 . '</h4><div class="checkbox">
 		  <label>
 			<input checked type="radio" required name="from" value="' . $_GET['from'] . '"> ' . $_GET['from'] . '
